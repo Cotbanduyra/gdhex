70449745228a6d0da8315e4e1894ba6e8dd62495
Cotbanduyra <101418509+Cotbanduyra@users.noreply.github.com>
2026-02-06 13:41:23 +0300

    Add files via upload

--- hex_prog.cpp
+++ hex_prog.cpp
@@ -8,6 +8,7 @@ int main(int argc, char** argv){
         long long int number;
         for (short i = 1; i < argc; i++){
             if (argv[i][1] == 'b')  number = stoll(argv[i], nullptr, 2);
+            else if (argv[i][1] == 'x') number = stoll(argv[i], nullptr, 16);
             else    number = stoll(argv[i]);
             
             cout << dec << argv[i] << "  --  ";
