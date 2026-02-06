a647e1718d0a2d7b6fa4dc3f673247b3945654a4
Cotbanduyra <101418509+Cotbanduyra@users.noreply.github.com>
2026-02-06 13:24:42 +0300

    Add files via upload
    initial commit

---
+++ hex_prog.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+    if (argc > 1){
+        
+        long long int number;
+        for (short i = 1; i < argc; i++){
+            if (argv[i][1] == 'b')  number = stoll(argv[i], nullptr, 2);
+            else    number = stoll(argv[i]);
+            
+            cout << dec << argv[i] << "  --  ";
+            cout << hex;
+            if (number < 0)
+                cout << '-' << "0x" << abs(number);
+            else
+                cout << "0x" << number;
+            cout << endl;
+        }
+    }
+}
